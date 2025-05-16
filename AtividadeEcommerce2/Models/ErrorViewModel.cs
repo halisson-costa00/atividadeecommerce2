ac1f401d76a4e7156a8cae088399ec4276170fd5
halisson.costa00@gmail.com <halisson.costa00@gmail.com>
2025-05-16 09:14:59 -0300

    criando AtividadeEcommerce2

---
+++ AtividadeEcommerce2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AtividadeEcommerce2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
